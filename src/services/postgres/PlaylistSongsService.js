a1052e5e40490df12f9ee7b34651f94106f8584b
Seprianto Sianipar <raysianipar15@gmail.com>
2021-11-11 13:58:27 +0700

    add song to playlist

--- src/services/postgres/PlaylistSongsService.js
+++ src/services/postgres/PlaylistSongsService.js
@@ -1,6 +1,7 @@
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class PlaylistSongsService {
   constructor() {
@@ -11,15 +12,43 @@ class PlaylistSongsService {
     const id = `song-${nanoid(16)}`;
     const query = {
       text: 'INSERT INTO playlistsongs VALUES($1, $2, $3) RETURNING id',
-      value: [id, playlistId, songId],
+      values: [id, playlistId, songId],
     };
 
     const result = await this._pool.query(query);
 
     if (!result.rowCount) {
-      throw new InvariantError('Song gagal ditambahkan ke dalam Playlist');
+      throw new InvariantError('Lagu gagal ditambahkan ke dalam playlist');
     }
     return result.rows[0].id;
+  }
+
+  async getPlaylistSong(playlistId) {
+    const query = {
+      text: 'SELECT p.id, s.title, s.performer FROM playlistsongs p INNER JOIN songs s ON p.song_id = s.id WHERE p.playlist_id = $1',
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Lagu tidak ditemukan di dalam playlist');
+    }
+
+    return result.rows;
+  }
+
+  async deletePlaylistSongById(playlistId, songId) {
+    const query = {
+      text: 'DELETE FROM playlistsongs WHERE playlistId = $1 AND songId = $2 RETURNING id',
+      values: [playlistId, songId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Lagu berhasil dihapus dari playlist');
+    }
   }
 }
 
