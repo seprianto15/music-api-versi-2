9622060fafc998cdc543b4f627f42fd4ac27d21e
Seprianto Sianipar <raysianipar15@gmail.com>
2021-11-16 20:13:40 +0700

    update collaboration server

--- src/server.js
+++ src/server.js
@@ -38,9 +38,9 @@ const init = async () => {
   const songsService = new SongsService();
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const playlistsService = new PlaylistsService();
-  const playlistsongsService = new PlaylistSongsService();
   const collaborationsService = new CollaborationsService();
+  const playlistsService = new PlaylistsService(collaborationsService);
+  const playlistsongsService = new PlaylistSongsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
